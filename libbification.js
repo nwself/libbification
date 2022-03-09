9ae5e21dde5c06167cc931002f34d7aa647107c7
Nathan Self <nwself@cs.vt.edu>
2022-03-08 20:24:50 -0500

    Add better messages

--- libbification.js
+++ libbification.js
@@ -13,7 +13,31 @@
     "https://librarypoint.overdrive.com",
     "https://mrl.overdrive.com",
     "https://finditva.overdrive.com",
-  ]
+    "https://nnpls.overdrive.com",
+
+    "https://chesapeake.overdrive.com",
+    "https://rvl.overdrive.com",
+    "https://princewilliam.overdrive.com",
+    "https://brdc.overdrive.com",
+    "https://swvapub.overdrive.com",
+    "https://vppl.overdrive.com",
+    "https://elprlibrary.overdrive.com",
+    "https://yorkcountyva.overdrive.com",
+  ];
+
+  const cards = {
+    "librarypoint": true,
+    "mrl": true,
+    "finditva": true,
+    "nnpls": true,
+  };
+
+  function format(library) {
+    if (library in cards) {
+      return "__" + library + "__";
+    }
+    return library;
+  }
 
   /**
    * Listen for messages from the background script.
@@ -26,7 +50,7 @@
       if (selectedText) {
         for (const basePath of libraries) {
           const library = basePath.substring(8, basePath.indexOf("."));
-          console.log(`Look for ${selectedText} at ${library}`)
+          console.log(`Look for ${selectedText} at ${format(library)}`)
           let myRequest = new Request(`${basePath}/search?query=${selectedText.replaceAll(" ", "+")}&format=ebook-overdrive&sortBy=relevance`);
 
           fetch(myRequest)
@@ -36,12 +60,14 @@
               console.log("got this error", error);
             })
             .then(function(html) {
+              let answer = "";
               var parser = new DOMParser();
               var doc = parser.parseFromString(html, 'text/html');
 
               const nothing = doc.getElementById("noresults");
               if (nothing) {
-                console.log(`No entry for ${selectedText} at ${library}`);
+                answer = `No entry for ${selectedText} at ${format(library)}`;
+                console.log(answer);
               } else {
                 var regex = /window\.OverDrive\.titleCollection.*\n/;
                 var head = doc.getElementsByTagName("head").item(0);
@@ -53,9 +79,18 @@
 
                 const results = books.forEach(function(book) {
                   const authors = book.creators.filter(c => c.role == "Author").map(c => c.name).join(", ")
-                  console.log(`${book.title} (${authors}) ${book.isAvailable ? "available" : "UNAVAILABLE"} ${basePath}/media/${book.id}`);
+                  answer = `${(library in cards) ? "__" : ""}${book.title} (${authors}) ${book.isAvailable ? "available" : "UNAVAILABLE"} ${basePath}/media/${book.id}`;
+                  console.log(answer);
                 });
               }
+
+              var newNode = document.createElement('p');
+              newNode.textContent = answer;
+              newNode.style.position = "absolute";
+              newNode.style.top = "0";
+
+              var referenceNode = document.querySelector('body');
+              referenceNode.append(newNode);
             })
         }
       }
